86a06014c4d99ac8e4a455349320d77cd9976d41
kojaa <kojic97stole@gmail.com>
2018-07-21 17:12:17 +0200

    api controller added

--- Routes.php
+++ Routes.php
@@ -6,5 +6,9 @@
         Route::get('|^category/([0-9]+)/?$|', 'Category', 'show'),
         Route::get('|^category/([0-9]+)/delete/?$|', 'Category', 'delete'),
         Route::get('|^auction/([0-9]+)/?$|', 'Auction', 'show'),
+
+        #api routes
+        Route::get('|^api/auction/([0-9]+)/?$|', 'ApiAuction', 'show'),
+
         Route::any('|^.*$|', 'Main', 'home')
     ];
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     require_once "Configuration.php";
     require_once "vendor/autoload.php";
 
+    ob_start();
+
     use App\Core\DatabaseConfiguration;
     use App\Core\DatabaseConnection;
     use App\Core\Router;
@@ -37,20 +39,18 @@
     $route = $router->find($httpMethod, $url);
     $arguments = $route->extractArguments($url);
     
-    // print_r($route);
-    // print_r($arguments);
-    // exit();
 
     $fullControllerName = "\\App\\Controllers\\". $route->getControllerName(). "Controller";
     $controller = new $fullControllerName($databaseConnection);
     call_user_func_array([$controller, $route->getMethodName()], $arguments);
     $data = $controller->getData();
 
-    // foreach($data as $name => $value){
-    //     $$name = $value;
-    // }
-
-    // require_once "views/". $route->getControllerName() ."/". $route->getMethodName().".php";
+    if($controller instanceof \App\Core\ApiController){
+        ob_clean();
+        header('Content-type: application/json; charset=utf-8');
+        echo json_encode($data);
+        exit;
+    }
 
     $loader = new Twig_Loader_Filesystem("./views");
 
